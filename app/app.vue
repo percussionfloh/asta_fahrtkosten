d10f56992bbcf417a72a38b604215bca1f07cb39
percussionfloh <florian.moldaschl@web.de>
2026-07-22 00:34:15 +0200

    update text on app.vue

--- app/app.vue
+++ app/app.vue
@@ -41,15 +41,6 @@ useHead({
       </div>
 
       <AppForm />
-      <!-- <div class="grid justify-items-center grid-cols-4 p-4 gap-3">
-        <UForm @submit="onSubmit">
-          <UFileUpload v-model="antrag" :dropzone="true" label="ausgefüllter Antrag" class="w-full" />
-          <UFileUpload v-model="rechnung" :dropzone="true" label="Rechnung(en), bitte als PDF zusammengefügt" class="w-full" />
-          <UFileUpload v-model="bestaetigung" :dropzone="true" label="Teilnahmebestätigung" class="w-full"/>
-          <UTextarea :rows="9" class="w-full border-red-500" placeholder="weitere Infos für den AStA"/>
-          <UButton label="Submit" />        
-        </UForm>
-      </div> -->
 
       <div class="p-7">
         <h2 class="text-2xl md:text-4xl text-red-600 font-bold">Was wird bezuschusst?</h2>
@@ -58,7 +49,8 @@ useHead({
         <p>
           (1) Mithilfe dieses Antrags können ausschließlich Fahrtkosten für Veranstaltungen bezuschusst werden, die der studienbezogenen Weiterbildung dienen. Dazu gehören u.a. Wettbewerbe, Meisterkurse, Probespiele, Fachvorträge und hochschulpolitische Veranstaltungen.
            Veranstaltungen, bei denen eine Gage ausgezahlt wird, werden nicht bezuschusst. Ob die beantragte Veranstaltung in diese Kategorien fällt obliegt der Entscheidung des AStA.
-          <br><br>(2) Bezuschusst werden Fahrtkosten bei Fahrten mit öffentlichen Verkehrsmitteln, mit eigenem oder mit geliehenem PKW. Fahrten in fremden PKWs können ebenfalls bezuschusst werden, Antragssteller:in muss aber immer die fahrende Person sein. 
+          <br><br>(2) Bezuschusst werden Fahrtkosten bei Fahrten mit öffentlichen Verkehrsmitteln, mit eigenem oder mit geliehenem PKW. Fahrten in fremden PKWs können ebenfalls bezuschusst werden, Antragssteller:in muss aber immer die fahrende Person sein.
+          <b>Diese sollten spätestens einen Monat nach dem Veranstaltungstermin eingereicht werden.</b> <br>
           Flüge werden nicht bezuschusst. Tickets für Nahverkehrsangebote, z. B. Stadttickets werden nicht bezuschusst. Das Deutschlandticket wird mit 20€ bezuschusst.
           <br><br>(3) Es können ausschließlich Fahrtkosten von immatrikulierten Studierenden der Hochschule für Musik Freiburg bezuschusst werden.
           <br><br>(4) Da dem AStA für Fahrtkosten nur ein begrenztes Budget zur Verfügung steht, ist die Bewilligung des Zuschusses sowie die Höhe der einzelnen Zuschüsse von der Anzahl der eingehenden Anträge abhängig. Ein Anspruch auf Bezuschussung der Fahrtkosten besteht nicht.
@@ -71,7 +63,7 @@ useHead({
       <div class="p-3">
         <p>
           Bei der Berechnung des Zuschusses gelten folgende Richtlinien: <br><br>
-          (1) Bezuschusst wird in der Regel der volle Fahrtkostenbetrag. Jede:r Student:in kann jedoch pro Semester maximal 100€ Zuschuss erhalten. Überweisungen unter 15€ werden nicht vorgenommen. <br><br>
+          (1) Bezuschusst wird in der Regel der volle Fahrtkostenbetrag. Jede*r Student*in kann jedoch pro Semester maximal 100€ Zuschuss erhalten. Überweisungen unter 15€ werden nicht vorgenommen. <br><br>
           (2) Bei Fahrten mit öffentlichen Verkehrsmitteln wird der Fahrpreis der 2. Klasse bezuschusst. <br><br>
           (3) Bei Fahrten mit eigenem sowie geliehenem PKW wird der Berechnung eine Kilometerpauschale von 30 ct zugrunde gelegt. <br><br>
           (4) Bei Fahrten, bei denen andere Studierende der Hochschule für Musik Freiburg mitgenommen wurden, wird der Fahrtkostenbetrag durch die Anzahl aller Personen im Auto geteilt und pro Person abgerechnet. Der komplette Zuschuss für alle Personen wird an die antragstellende Person ausgezahlt.
@@ -89,8 +81,9 @@ useHead({
             (4) Die Fahrt darf frühestens zwei Tage vor Beginn und spätestens zwei Tage nach Ende der Veranstaltung angetreten werden. <br><br>
             (5) Der Antrag auf einen Zuschuss muss innerhalb eines Monats nach Ende der Veranstaltung schriftlich mit dem dafür vorgesehenen Formular beim AStA eingereicht werden. Alle notwendigen Belege müssen zu dieser Frist eingereicht sein, sonst ist der Antrag ungültig. <br><br>
             (6) Folgende Dokumente sind beim AStA einzureichen: <br><br>
-            a. Der ausgefüllte Antrag (Seite 1 und 2) <br><br>
-            b. Eine Teilnahmebestätigung für die entsprechende Veranstaltung (Kopie) mit Bestätigung, dass keine bzw. wie viel Fahrtkosten bereits erstattet wurden <br><br>
+            a. Der ausgefüllte Antrag (Seite 1 und 2), bitte in Blockschrift ausfüllen. Alles muss gut lesbar sein, damit dein Antrag korrekt bearbeitet werden kann.<br><br>
+            b. Eine Teilnahmebestätigung für die entsprechende Veranstaltung (Kopie) mit Bestätigung, dass keine bzw. wie viel Fahrtkosten bereits erstattet wurden <br>    Wir akzeptieren eine Muvac-Einladung zum Probespiel nicht als gültige Teilnahmebestätigung.
+              Als Teilnahmebestätigung akzeptieren wir ausschließlich offizielle Schreiben des Veranstalters (bitte kontaktiere hierfür das jeweilige Orchester- oder Wettbewerbsbüro) oder Fotos, auf denen eindeutig erkennbar ist, dass du an der Veranstaltung teilgenommen hast. <br><br>
             c. Bei einer Fahrt mit öffentlichen Verkehrsmitteln: Die Fahrkarte im Original, der Betrag muss ersichtlich sein <br><br>
             d. Bei einer Fahrt mit eigenem PKW: Der Tankbeleg im Original <br><br>
             e. Bei einer Fahrt mit geliehenem PKW: Rechnung des Anbieters mit Kilometeranzahl <br><br>
